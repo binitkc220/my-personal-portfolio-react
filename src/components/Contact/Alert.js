985c28f0249794e372fd55564e7933ec4e2d4e83
Binit KC <binitkc220@gmail.com>
2022-11-07 10:22:19 +0545

    class changed to className

--- src/components/Contact/Alert.js
+++ src/components/Contact/Alert.js
@@ -3,7 +3,7 @@ import './Contact.css';
 
 export default function Alert(props) {
     return (
-        props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show alert-mssg m-3 p-3`} role="alert">
+        props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show alert-mssg m-3 p-3`} role="alert">
             {props.alert.msg}
         </div>
     )
